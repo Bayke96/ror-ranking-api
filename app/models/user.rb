1c38f537c3d7b5e267b1141d47a167d92313d7ad
Cristian González <41243425+Bayke96@users.noreply.github.com>
2019-08-17 13:21:35 -0400

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
 
   # Class variables
   attribute :id, :integer
+  attribute :department_id, :integer
+  attribute :rank_id, :integer
   attribute :name, :string
   attribute :password, :string
 
   # Validators
-  validates :departmentfk, presence: true, numericality: { only_integer: true }
-  validates :rankfk, presence: true, numericality: { only_integer: true }
+  validates :department_id, presence: true, numericality: { only_integer: true }
+  validates :rank_id, presence: true, numericality: { only_integer: true }
   validates :name, presence: true, uniqueness: { message: "The user's name must be unique"}, length: { in: 3..128 }
   validates :password, presence: true, length: { in: 12..128 }
 
